84f93601d12e7bbe5e65ac4b538575a11c2e512f
Santiago <santiago.dev21@gmail.com>
2025-06-25 17:21:31 -0300

    Se agrega pantalla de Save Password

--- app/src/main/java/com/santidev/passwordmanager/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/santidev/passwordmanager/navigation/NavigationWrapper.kt
@@ -4,8 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.santidev.passwordmanager.navigation.SavePassword
 import com.santidev.passwordmanager.ui.screens.MainScreen
+import com.santidev.passwordmanager.ui.screens.SavePassword
 import com.santidev.passwordmanager.ui.screens.SplashScreen
+import com.santidev.passwordmanager.utils.PasswordGeneratorScreen
 
 @Composable
 fun NavigationWrapper() {
@@ -17,6 +20,9 @@ fun NavigationWrapper() {
     }
     composable<MainScreen> {
       MainScreen(navController = navController)
+    }
+    composable<SavePassword> {
+      SavePassword(navController = navController)
     }
   }
   
---
+++ app/src/main/java/com/santidev/passwordmanager/ui/screens/SavePassword.kt
@@ -0,0 +1,10 @@
+package com.santidev.passwordmanager.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SavePassword(navController: NavHostController) {
+  Text("pantalla de guardado de contraseña")
+}
