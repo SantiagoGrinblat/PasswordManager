290ffcb33f1ba78eb8a72b25b78606a06774768b
Santiago <santiago.dev21@gmail.com>
2025-06-26 20:16:02 -0300

    se agrega librerias para la lista con koin, ksp y room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.jetbrains.kotlin.serialization)
+  alias(libs.plugins.ksp)
 }
 
 android {
@@ -45,6 +46,14 @@ dependencies {
   
   implementation(libs.androidx.appcompat)
   
+  implementation(project.dependencies.platform(libs.koin.bom))
+  implementation(libs.koin.android)
+  implementation(libs.koin.core)
+  implementation(libs.koin.androidx.compose)
+  
+  implementation(libs.room.ktx)
+  implementation(libs.room.runtime)
+  ksp(libs.room.compiler)
   
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
