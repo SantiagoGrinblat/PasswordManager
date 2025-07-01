7e3ebbca071a6d603b4e8804d11916e54cf0caec
Santiago <santiago.dev21@gmail.com>
2025-07-01 18:28:37 -0300

    Se modifica importaciones de BiometricHelper

--- app/src/main/java/com/santidev/passwordmanager/biometric/BiometricHelper.kt
+++ app/src/main/java/com/santidev/passwordmanager/biometric/BiometricHelper.kt
@@ -1,81 +1,35 @@
 package com.santidev.passwordmanager.biometric
 
-import android.content.Context
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
-import java.security.MessageDigest
 
 object BiometricHelper {
-
+  
   fun createBiometricPrompt(
     activity: AppCompatActivity,
     processSuccess: (BiometricPrompt.AuthenticationResult) -> Unit
   ): BiometricPrompt {
     val executor = ContextCompat.getMainExecutor(activity)
-
+    
     val callback = object : BiometricPrompt.AuthenticationCallback() {
       override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
         super.onAuthenticationError(errorCode, errString)
       }
-
+      
       override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
         super.onAuthenticationSucceeded(result)
         processSuccess(result)
       }
-
+      
       override fun onAuthenticationFailed() {
         super.onAuthenticationFailed()
       }
     }
-
+    
     return BiometricPrompt(activity, executor, callback)
   }
-
+  
   fun createPromptInfo(): BiometricPrompt.PromptInfo =
     BiometricPrompt.PromptInfo.Builder().apply {
       setTitle("Autenticación biometrica")
@@ -83,7 +37,7 @@ object BiometricHelper {
       setConfirmationRequired(false)
       setNegativeButtonText("Cancelar")
     }.build()
-
+  
   fun showPrompt(activity: AppCompatActivity, onSuccess: () -> Unit) {
     createBiometricPrompt(activity = activity) {
       onSuccess()
